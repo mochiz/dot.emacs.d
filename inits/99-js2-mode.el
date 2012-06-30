3c11d460128a5f2c3e2a609492987299b3e5b693
Hiroshi Asakura <mochi.hiro43@gmail.com>
2012-07-01 08:02:06 +0900

    replace js2-mode

--- inits/99-js2-mode.el
+++ inits/99-js2-mode.el
@@ -1,8 +1,10 @@
 ;; javascript mode
 ;; install from epla
+(add-to-list 'load-path "~/.emacs.d/vendor/js2-mode")
 (require 'js2-mode)
 (defun javascript-custom ()
   "javascript-mode-hook"
   (set (make-local-variable 'tab-width) 2))
 (add-hook 'javascript-mode-hook
           '(lambda() (javascript-custom)))
+(add-to-list 'auto-mode-alist '("\\.\\(js\\|json\\)$" . js2-mode))
