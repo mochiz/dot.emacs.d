771d52e282da8e6a9789a3a7a6fa43eb5d21cc3c
Hiroshi Asakura <mochi.hiro43@gmail.com>
2012-11-12 07:47:43 +0900

    modify inits/sense-region.el

--- inits/99-sense-region.el
+++ inits/99-sense-region.el
@@ -1,4 +1,5 @@
-;; sense-region
+;; install-elisp-from-gist RET https://gist.github.com/1776988
+;; sense-region.el
 (autoload 'sense-region-on "sense-region"
   "System to toggle region and rectangle." t nil)
 (sense-region-on)
